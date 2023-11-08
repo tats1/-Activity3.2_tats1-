b0d4158d9ae9468d0d498e55da708acafa54abbc
tats1 <lawrencekeithlicauan@gmail.com>
2023-11-08 08:07:16 +0800

    “feat/add-column-price”

--- hero.sql
+++ hero.sql
@@ -154,3 +154,7 @@ VALUES
     (8, 8, 108),  -- Fanny has Thunder Belt
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
+
+
+ALTER TABLE public.item
+ADD item_price DECIMAL(10, 2);
